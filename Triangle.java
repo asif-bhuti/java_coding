046e1042b2507e37b086fc7090b38711dcb63932
asif-bhuti <asif.vudi@northsouth.edu>
2022-09-06 10:16:53 +0600

    Illegal Triangle Exception Handling

---
+++ Triangle.java
@@ -0,0 +1,36 @@
+class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String str) {
+        super(str);
+    }
+}
+
+public class Triangle {
+
+    static void validateTriangle(int a, int b, int c) throws IllegalTriangleException {
+
+        if (a >= b + c)
+            throw new IllegalTriangleException("Invalid Triangle.");
+        else if (b >= a + c)
+            throw new IllegalTriangleException("Invalid Triangle.");
+        else if (c >= b + a)
+            throw new IllegalTriangleException("Invalid Triangle.");
+        else {
+            System.out.println("Valid");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            // calling the method
+            validateTriangle(1, 2, 5);
+            ;
+        } catch (IllegalTriangleException ex) {
+            System.out.println("Caught the exception");
+
+            // printing the message from InvalidAgeException object
+            System.out.println("Exception occured: " + ex);
+        }
+
+    }
+
+}
